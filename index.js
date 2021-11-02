f06d669bb6269c43e28c985a8fe176049a621e44
Mohammad Islam <islam.mohammad.student@gmail.com>
2021-11-02 14:11:04 +0000

    finished q1-2

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 //1
-function areBothSame(){
-  
+function areBothSame(para1, para2){
+  if (para1 == para2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //2
-function areBothEqual(){
-  
+function areBothEqual(para1, para2){
+  if  (para1 === para2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //3
@@ -25,3 +33,7 @@ function areAllFourSameOrEqual(){
 }
 
 
+//invoking functions
+
+//console.log(areBothSame("5", 1));
+//console.log(areBothEqual("2", 2));
